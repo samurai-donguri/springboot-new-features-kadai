58e1267f2d802169908b676f1ef4a7d4dd70c860
samurai-donguri <161983460+samurai-donguri@users.noreply.github.com>
2024-04-29 13:22:57 -0700

    Delete kadai_002/samuraitravel/src directory

--- kadai_002/samuraitravel/src/main/resources/static/js/stripe.js
+++
@@ -1,8 +0,0 @@
-const stripe = Stripe('pk_test_51P4WLNP0nsLMqosbS0JSYLcRHhT6TmVqVgK9pLRGJOCJhtXNOiN8efw76a2k1jrpNsC1jcD7cCKFOZ6HrGLGe7wp009Ey24jbg');
-const paymentButton = document.querySelector('#paymentButton');
-
-paymentButton.addEventListener('click', () => {
-  stripe.redirectToCheckout({
-    sessionId: sessionId
-  })
-});
